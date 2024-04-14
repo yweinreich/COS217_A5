edaeddeef939376e38f7f78ea59d043156d05ac9
Ariella Pressner <ap0760@princeton.edu>
2024-04-14 17:48:15 -0400

    .s code

--- mywc.s
+++ mywc.s
@@ -8,6 +8,8 @@
 
 //--------------------------------------------------------------------
     .section .rodata
+newLineStr:
+        .string "\n"
 printfFormatStr:
         .string "%7ld %7ld %7ld\n"
 
@@ -46,6 +48,12 @@ iChar:
 
 // must be multiple of 16, don't ask
 .equ MAIN_STACK_BYTECOUNT, 16
+// give registers meaningful names
+currentChar .req x1
+charCount   .req x2
+wordCount   .req x3
+lineCount   .req x4
+
 
 .global main
 
@@ -59,20 +67,22 @@ whileLoop:
     // if ((iChar = getchar()) == EOF) goto endWhileLoop;
         bl      getchar
         adr     x1, iChar
-        ldr     w0, [x1]
+        str     w0, [x1]
         cmp     [x1], EOF
         beq endWhileLoop
 
     // lCharCount++;
-        adr     x2, lCharCount
-        ldr     x2, [x2]
+        adr     x0, lCharCount
+        ldr     x2, [x0]
         // add .equ for incrementation?
         add     x2, x2, 1
+        str     x2, [x0]
 
     // if (!isspace(iChar)) goto elseSpace;
-        adr     x0, iChar
-        ldr     w0, [x0]
+        adr     x1, iChar
+        ldr     w0, [x1]
         bl      isspace
+        // result of isspace will overwrite iChar in w0
         cmp     w0, FALSE
         beq     elseSpace
 
@@ -83,9 +93,10 @@ whileLoop:
         beq     endIfWord
 
     // lWordCount++;
-        adr     x4, lWordCount
-        ldr     x4, [x4]
+        adr     x0, lWordCount
+        ldr     x4, [x0]
         add     x4, x4, 1
+        str     x4, [x0]
     // iInWord = FALSE;
         mov     w3, FALSE
     // goto endIfWord;
@@ -93,24 +104,61 @@ whileLoop:
 
     elseSpace:
 
-        if (iInWord)
-            goto endIfWord;
-        iInWord = TRUE;
+    // if (iInWord) goto endIfWord;
+        cmp     w3, TRUE
+        beq     endIfWord
 
-        endIfWord:
+    // iInWord = TRUE;
+        mov     w3, TRUE
 
-        if (iChar != '\n')
-            goto whileLoop;
-        lLineCount++;
+    endIfWord:
 
-        goto whileLoop;
+    // if (iChar != '\n') goto whileLoop;
+        adr     x0, iChar
+        ldr     w1, [x0]
+        adr     x0, newLineStr
+        ldr     w5, [x0]
+        cmp     w1, w5
+        bne     whileLoop
 
-        endWhileLoop:
-        if (!iInWord)
-            goto endIfWord2;
+    // lLineCount++;
+        adr     x0, lLineCount
+        ldr     x6, [x0]
+        add     x6, x6, 1
+        str     x6, [x0]
+
+    // goto whileLoop;
+        b whileLoop
+
+    endWhileLoop:
+    // if (!iInWord) goto endIfWord2;
+        adr     x0, iInWord
+        ldr     x3, [x0]
+        cmp     x3, FALSE
+        beq     endIfWord2
+        
         lWordCount++;
+        adr     x0, lWordCount
+        ldr     x4, [x0]
+        add     x4, x4, 1
+        str     x4, [x0]
 
         endIfWord2:
-        printf("%7ld %7ld %7ld\n", lLineCount, lWordCount, lCharCount);
-        return 0;
+    // printf("%7ld %7ld %7ld\n", lLineCount, lWordCount, lCharCount);
+        adr     x0, printfFormatStr
+        adr     x1, lLineCount
+        ldr     x1, [x1]
+        adr     x2, lWordCount
+        ldr     x2, [x2]
+        adr     x3, lCharCount
+        ldr     x3, [x3]
+        bl      printf
+    
+    // epilog and return 0;
+        mov w0, 0
+        ldr x30, [sp]
+        add sp, sp, MAIN_STACK_BYTECOUNT
+        ret
+
+        .size main, (. -main)
 
